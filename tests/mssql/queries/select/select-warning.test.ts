c8ad72c784078fa3743c382d3cb7859714428b27
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-03 13:13:57 +0200

    Test having warning

--- tests/mssql/queries/select/select-warning.test.ts
+++ tests/mssql/queries/select/select-warning.test.ts
@@ -1,3 +1,5 @@
+import { ConditionType } from "../../../../src/chips-lq/types/conditions/condition-type.enum";
+import { ConditionOperands } from "../../../../src/chips-lq/types/conditions/operands/condition-operands.enum";
 import { QueryTypes } from "../../../../src/chips-lq/types/queries/query.type";
 import { ValueTypes } from "../../../../src/chips-lq/types/values/value.type";
 import { SqlLanguages } from "../../../../src/sql/sql-languages.enum";
@@ -28,5 +30,38 @@ service.expectWarning(
   {
     level: WarningLevels.EXECUTION_WILL_FAIL,
     message: "ORDER BY is required when using OFFSET",
+  }
+);
+
+service.expectWarning(
+  "GROUP BY is required when using HAVING",
+  {
+    queryType: QueryTypes.SELECT,
+    fields: [
+      {
+        valueType: ValueTypes.ALL_COLUMNS,
+      },
+    ],
+    from: [
+      {
+        name: "customers",
+      },
+    ],
+    having: {
+      conditionType: ConditionType.CONDITION,
+      conditionOperand: ConditionOperands.EQUALS,
+      sourceValue: {
+        valueType: ValueTypes.COLUMN,
+        field: "phone",
+      },
+      targetValue: {
+        valueType: ValueTypes.RAW_VALUE,
+        value: "555 xx xx xx",
+      },
+    },
+  },
+  {
+    level: WarningLevels.EXECUTION_WILL_FAIL,
+    message: "GROUP BY is required when using HAVING",
   }
 );
