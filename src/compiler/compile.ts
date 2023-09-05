37dede2bdf17706d0c3abc58aa81231519deae26
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-05 21:30:59 +0200

    Fix: wrong params

--- src/compiler/compile.ts
+++ src/compiler/compile.ts
@@ -1,9 +1,7 @@
 import { Query } from "../chips-lq/types/queries/query.type";
 import { SqlLanguages } from "../sql/sql-languages.enum";
-import { UnavailableFeatureError } from "../errors/compiler/unavailable-feature.error";
 import { QueryCompiler } from "./query/query.compiler";
 
 export const compile = (query: Query<Object>, language: SqlLanguages) => {
-    return new QueryCompiler(query, language).compile();
-    throw new UnavailableFeatureError();
-}
+  return new QueryCompiler(language).compile(query);
+};
