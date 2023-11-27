8845d65e342c321d12c8a58868528f67cc4e0d19
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-27 19:30:52 +0100

    Instantiate update test file

---
+++ tests/mssql/queries/update/update.test.ts
@@ -0,0 +1,5 @@
+import { SqlLanguages } from "../../../../src/sql/sql-languages.enum";
+import { TestService } from "../../../test.service";
+
+const service = new TestService(SqlLanguages.MSSQL);
+
